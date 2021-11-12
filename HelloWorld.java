91140e45390acf472dd0cb2239a9fe0c4d9b61c2
roadpilot <mdacht777@gmail.com>
2021-11-12 13:32:02 -0700

    print bool

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,5 +8,6 @@ public class HelloWorld {
     public static void main(String args[]){
         System.out.println("Hello, World!");
         System.out.println(); //empty line
+        System.out.println(true); //boolean
     }
 }
