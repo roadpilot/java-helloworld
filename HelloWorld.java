b20ac2787bedde436a4fca54c70893edf7c78bd8
roadpilot <mdacht777@gmail.com>
2021-11-12 13:31:22 -0700

    empty line

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,5 +7,6 @@ public class HelloWorld {
     // main method
     public static void main(String args[]){
         System.out.println("Hello, World!");
+        System.out.println(); //empty line
     }
 }
