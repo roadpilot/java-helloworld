b7b7436d84558fb4ffaaa4611464dfd1e184d19b
roadpilot <mdacht777@gmail.com>
2021-11-12 13:38:49 -0700

    missing semicolon

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,13 +3,13 @@ Multiline comment
 */
 // Single line comment
 
-Public class HelloWorld {
+public class HelloWorld {
     // main method
     public static void main(String args[]){
         System.out.println("Hello, World!");
         System.out.println(); //empty line
         System.out.println(true); //boolean
         System.out.println(25); //numeric
-        System.err.println("Some error message");
+        System.err.println("Some error message")
     }
 }
