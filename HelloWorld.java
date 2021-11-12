847f3ae57a22955a90d04ccefb9439bd3aea4a6c
roadpilot <mdacht777@gmail.com>
2021-11-12 13:16:35 -0700

    documenting

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ Multiline comment
 // Single line comment
 
 public class HelloWorld {
+    // main method
     public static void main(String args[]){
         System.out.println("Hello, World!");
     }
